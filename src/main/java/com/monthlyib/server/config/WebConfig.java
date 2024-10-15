ba965e8ff2de4e85d976a55ed42d0549265df8ab
Monthlyib <monthlyib@gmail.com>
2024-10-15 13:11:03 +0900

    CORS

--- src/main/java/com/monthlyib/server/config/WebConfig.java
+++ src/main/java/com/monthlyib/server/config/WebConfig.java
@@ -21,4 +21,14 @@ public class WebConfig implements WebMvcConfigurer {
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(userSessionResolver);
     }
+
+    @Override
+    // CORS 설정 추가
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")  // 모든 경로에 대해 CORS 허용
+                .allowedOrigins("https://www.monthly-ib.com")  // 허용할 출처
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // 허용할 HTTP 메서드
+                .allowedHeaders("*")  // 모든 헤더 허용
+                .allowCredentials(true);  // 쿠키 및 인증 정보 허용
+    }
 }
